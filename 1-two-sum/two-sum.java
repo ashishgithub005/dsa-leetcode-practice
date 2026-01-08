4a2b2e713a7b9e5d4373c9b150e4f31886a120d1
ashishgithub005 <160697963+ashishgithub005@users.noreply.github.com>
2026-01-08 23:11:09 +0530

    Time: 51 ms (5.55%) | Memory: 47.4 MB (11.36%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,7 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = optamized(nums, target);
+        int[] res = bruteForce(nums, target);
+        //int[] res = optamized(nums, target);
         return res;
+    }
+
+    public int[] bruteForce(int[] nums, int target){
+        int[] res = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int sum = nums[i] + nums[j];
+                if(sum == target){
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+     
+       return res;
     }
 
     public int[] optamized(int[] nums, int target){
